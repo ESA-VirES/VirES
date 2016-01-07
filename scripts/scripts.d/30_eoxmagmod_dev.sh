578d11a6a0c7ac6e8e016a7fdecc5dc293a9a872
Martin Paces <martin.paces@eox.at>
2016-01-07 17:53:37 +0100

    Adding EOxMagMod installer.

--- scripts/scripts.d/30_eoxmagmod_dev.sh
+++ scripts/scripts.d/30_eoxmagmod_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #-------------------------------------------------------------------------------
 #
-# Purpose: EOX server installation - development mode
+# Purpose: EOX magnetic model library - development mode
 # Author(s): Martin Paces <martin.paces@eox.at>
 #-------------------------------------------------------------------------------
 # Copyright (C) 2015 EOX IT Services GmbH
@@ -11,15 +11,24 @@
 info "Installing EOxServer in the development mode."
 
 # Path to the EOxServer development directory tree:
-EOXS_DEV_PATH="${EOXS_DEV_PATH:-/usr/local/eoxserver}"
-
+EOXMM_DEV_PATH="${EOXMM_DEV_PATH:-/usr/local/eoxmagmod}"
 
 # STEP 1: INSTALL DEPENDENCIES
-yum --assumeyes install python-dateutil python-lxml mapserver mapserver-python proj-epsg
+yum --assumeyes install gcc-gfortran
 
-# STEP 2: INSTALL EOXSERVER
+# STEP 2: INSTALL EOXMM
 # Install EOxServer in the development mode.
 pushd .
-cd $EOXS_DEV_PATH
+cd $EOXMM_DEV_PATH
+# build dependencies
+pushd .
+cd eoxmagmod/geomaglib
+make clean build install
+popd
+pushd .
+cd eoxmagmod/qdipolelib
+make clean build install
+popd
+# install python package
 python ./setup.py develop
 popd
