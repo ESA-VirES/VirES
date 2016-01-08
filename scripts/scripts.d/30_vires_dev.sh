9035592895277f8edd347701f17eb7cc27085cf2
Martin Paces <martin.paces@eox.at>
2016-01-08 15:41:21 +0100

    Adding ViRES-Server installer.

---
+++ scripts/scripts.d/30_vires_dev.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#-------------------------------------------------------------------------------
+#
+# Purpose: ViRES server - development mode
+# Author(s): Martin Paces <martin.paces@eox.at>
+#-------------------------------------------------------------------------------
+# Copyright (C) 2015 EOX IT Services GmbH
+
+. `dirname $0`/../lib_logging.sh
+
+info "Installing ViRES-Server in the development mode."
+
+# Path to the ViRES-Server development directory tree:
+VIRES_DEV_PATH="${VIRES_DEV_PATH:-/usr/local/vires}"
+
+# STEP 1: INSTALL DEPENDENCIES
+yum --assumeyes install python-matplotlib
+
+# STEP 2: INSTALL VIRES
+# Install ViRES-SERVER in the development mode.
+pushd .
+cd $VIRES_DEV_PATH
+python ./setup.py develop
+popd
