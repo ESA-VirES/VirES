848fce710383ba6ed23b139b2e5f275f6f610faf
Martin Paces <martin.paces@eox.at>
2016-02-19 10:20:14 +0100

    Fixing the EOxMagMod installer.

--- scripts/scripts.d/30_eoxmagmod_dev.sh
+++ scripts/scripts.d/30_eoxmagmod_dev.sh
@@ -8,27 +8,29 @@
 
 . `dirname $0`/../lib_logging.sh
 
-info "Installing EOxMagMod in the development mode."
+#info "Installing EOxMagMod in the development mode."
+info "Installing EOxMagMod from sources ..."
 
 # Path to the EOxMagMod development directory tree:
 EOXMM_DEV_PATH="${EOXMM_DEV_PATH:-/usr/local/eoxmagmod}"
 
 # STEP 1: INSTALL DEPENDENCIES
-yum --assumeyes install gcc-gfortran python-setuptools
+#yum --assumeyes install gcc-gfortran python-setuptools
+yum --assumeyes install gcc-gfortran python-devel numpy
 
 # STEP 2: INSTALL EOXMM
 # Install EOxMagMod in the development mode.
 pushd .
 cd $EOXMM_DEV_PATH
 # build dependencies
-pushd .
-cd eoxmagmod/geomaglib
-make clean build install
-popd
+#pushd .
+#cd eoxmagmod/geomaglib
+#make clean build install
+#popd
 pushd .
 cd eoxmagmod/qdipolelib
 make clean build install
 popd
 # install python package
-python ./setup.py develop
+python ./setup.py install
 popd
