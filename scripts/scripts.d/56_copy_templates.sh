9db44d69d935db607d4dfc59917e2b5b374a76b4
Daniel Santillan <daniel.santillan@eox.at>
2016-03-03 17:19:19 +0100

    Modified script to accept templates source directory through variable.

--- scripts/scripts.d/56_copy_templates.sh
+++ scripts/scripts.d/56_copy_templates.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
 #-------------------------------------------------------------------------------
 #
-# Purpose: EOxServer instance configuration
+# Purpose: Load fixtures to the EOxServer instance.
 # Author(s): Martin Paces <martin.paces@eox.at>
 #            Daniel Santillan <daniel.santillan@eox.at>
 #-------------------------------------------------------------------------------
 # Copyright (C) 2015 EOX IT Services GmbH
 
+
 . `dirname $0`/../lib_logging.sh
 
-info "Copying instance templates ... "
+info "Copy available templates ... "
 
-
-# NOTE: Multiple EOxServer instances are not foreseen in VIRES.
-
+[ -z "$CONTRIB_DIR" ] && error "Missing the required CONTRIB_DIR variable!"
 [ -z "$VIRES_SERVER_HOME" ] && error "Missing the required VIRES_SERVER_HOME variable!"
 [ -z "$VIRES_USER" ] && error "Missing the required VIRES_USER variable!"
 
 
 INSTANCE="`basename "$VIRES_SERVER_HOME"`"
 INSTROOT="`dirname "$VIRES_SERVER_HOME"`"
-
-TEMPLATES_SRC="/usr/local/vires-dempo_ops/templates"
-TEMPLATES="${INSTROOT}/${INSTANCE}/${INSTANCE}/templates"
-
-#-------------------------------------------------------------------------------
-# STEP 1: COPY INSTANCE SPECIFIC TEMPLATES
-sudo -u "$VIRES_USER" cp -r "$TEMPLATES_SRC/." "$TEMPLATES"
+VAGRANT_SRC="/usr/local/vires-dempo_ops/templates"
+TEMPLATES_DIR_SRC="${TEMPLATES_DIR_SRC:-$VAGRANT_SRC}"
+TEMPLATES_DIR_DST="${INSTROOT}/${INSTANCE}/${INSTANCE}/templates"
 
 
+sudo -u "$VIRES_USER" cp -r "$TEMPLATES_DIR_SRC/." "$TEMPLATES_DIR_DST"
