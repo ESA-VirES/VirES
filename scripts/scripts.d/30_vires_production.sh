5dbc12d86322d3166e1623deef641d3612c5e782
Martin Paces <martin.paces@eox.at>
2016-03-08 16:55:22 +0100

    Fixing VirES-Server installation.

--- scripts/scripts.d/30_vires_production.sh
+++ scripts/scripts.d/30_vires_production.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #-------------------------------------------------------------------------------
 #
-# Purpose: VirES server - development mode
+# Purpose: VirES server - production mode
 # Author(s): Martin Paces <martin.paces@eox.at>
 #-------------------------------------------------------------------------------
 # Copyright (C) 2015 EOX IT Services GmbH
 
 . `dirname $0`/../lib_logging.sh
 
-info "Installing VirES-Server in the development mode."
+info "Installing VirES-Server packages."
 
 # Path to the VirES-Server development directory tree:
 VIRES_DEV_PATH="${VIRES_DEV_PATH:-/usr/local/vires}"
@@ -17,8 +17,14 @@ VIRES_DEV_PATH="${VIRES_DEV_PATH:-/usr/local/vires}"
 yum --assumeyes install python-matplotlib python-setuptools
 
 # STEP 2: INSTALL VIRES
-# Install VirES-SERVER in the development mode.
+# Install VirES EOxServer extension
 pushd .
-cd $VIRES_DEV_PATH
+cd "$VIRES_DEV_PATH/vires"
+python ./setup.py install
+popd
+
+# STEP 3: INSTALL EOxServer django-allauth integration
+pushd .
+cd "$VIRES_DEV_PATH/eoxs_allauth"
 python ./setup.py install
 popd
