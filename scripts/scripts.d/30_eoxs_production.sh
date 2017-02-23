6a7172e4a8a355cafe7d8a792a3272ab7080bdb6
Martin Paces <martin.paces@eox.at>
2017-02-23 10:38:32 +0100

    EOxServer install: removing any leftovers from the previous version.

--- scripts/scripts.d/30_eoxs_production.sh
+++ scripts/scripts.d/30_eoxs_production.sh
@@ -19,7 +19,11 @@ yum --assumeyes install python-dateutil python-lxml proj-epsg python-setuptools
 
 # STEP 2: INSTALL EOXSERVER
 # Install EOxServer in the development mode.
+PACKAGE=EOxServer
+[ -z "`pip freeze | grep "$PACKAGE==" `" ] || pip uninstall -y "$PACKAGE"
 pushd .
 cd $EOXS_DEV_PATH
+[ ! -d build/ ] || rm -fvR build/
+[ ! -d dist/ ] || rm -fvR dist/
 python ./setup.py install
 popd
