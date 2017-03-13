4f90ce95e03d72a2d43b9359626c44d0c761c46e
Martin Paces <martin.paces@eox.at>
2017-03-13 09:56:24 +0100

    Update of the WPS backend.

--- scripts/scripts.d/35_eoxs_wps_async.sh
+++ scripts/scripts.d/35_eoxs_wps_async.sh
@@ -10,7 +10,7 @@
 
 info "Installing EOxServer asynchronous WPS backend."
 
-SOURCE_URL="https://github.com/DAMATS/WPS-Backend/archive/0.2.1.tar.gz"
+SOURCE_URL="https://github.com/DAMATS/WPS-Backend/archive/0.3.0.tar.gz"
 
 # set temporary directory removed after the installation
 TMP_DIR="`mktemp -d`"
