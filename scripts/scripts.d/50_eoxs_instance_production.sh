34ff14391cf15b6f7436535534aa499c73afe0e0
Daniel Santillan <daniel.santillan@eox.at>
2017-10-23 10:31:43 +0200

    Set also admin mail handler to ERROR level

--- scripts/scripts.d/50_eoxs_instance_production.sh
+++ scripts/scripts.d/50_eoxs_instance_production.sh
@@ -338,7 +338,7 @@ LOGGING = {
             'filters': [],
         },
         'mail_admins': {
-            'level': 'WARNING',
+            'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler',
             'formatter': 'default',
             'filters': [],
