7f7279795135037e12a08cc3c7e285cf2c2907b6
Martin Paces <martin.paces@eox.at>
2018-04-06 16:50:02 +0200

    Improving eoxmagmod installation in the development mode.

--- scripts/scripts.d/30_eoxmagmod_devel.sh
+++ scripts/scripts.d/30_eoxmagmod_devel.sh
@@ -9,10 +9,15 @@
 . `dirname $0`/../lib_logging.sh
 . `dirname $0`/../lib_virtualenv.sh
 
+EOXMAGMOD_SOURCE_PATH="${EOXMAGMOD_SOURCE_PATH:-/usr/local/eoxmagmod}"
+
 info "Installing EOxMagMod in development mode ..."
 
 activate_virtualenv
 
 yum --assumeyes install wmm2015-lib qdipole wmm2015-devel qdipole-devel
 
-pip install -e "${EOXMAGMOD_SOURCE_PATH:-/usr/local/eoxmagmod}"
+# get rid of the previous build
+[ -d "$EOXMAGMOD_SOURCE_PATH/build" ] && rm -fvR "$EOXMAGMOD_SOURCE_PATH/build"
+[ -d "$EOXMAGMOD_SOURCE_PATH/dist" ] && rm -fvR "$EOXMAGMOD_SOURCE_PATH/dist"
+pip install -e "$EOXMAGMOD_SOURCE_PATH"
