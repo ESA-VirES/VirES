b54c7924c69ecdfb8ec44ffad9d63e61173eca2c
Martin Paces <martin.paces@eox.at>
2019-06-24 16:58:26 +0200

    Execute manage.py venv wrappers with arbitrary user.

--- scripts/virtualenv_manage.sh
+++ scripts/virtualenv_manage.sh
@@ -6,4 +6,14 @@
 [ -f "`dirname $0`/user.conf" ] && . `dirname $0`/user.conf
 . `dirname $0`/lib_common.sh
 
-sudo -u "$VIRES_USER" `dirname $0`/virtualenv_execute.sh python "$VIRES_SERVER_HOME/manage.py" "$@"
+if [ "$1" == '-u' ]
+then
+    USER="$2"
+    shift 2
+else
+    USER="$VIRES_USER"
+fi
+
+[ -z "$USER" ] && { echo "ERROR: No user given!" >&2 ; exit 2 ; }
+
+sudo -u "$USER" `dirname $0`/virtualenv_execute.sh python "$VIRES_SERVER_HOME/manage.py" "$@"
