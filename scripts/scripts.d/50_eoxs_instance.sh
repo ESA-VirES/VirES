ce591cec92af637ff5de507e7755a1ff317e618d
Martin Paces <martin.paces@eox.at>
2019-10-23 19:12:13 +0200

    Service Terms and Privacy Notice - links

--- scripts/scripts.d/50_eoxs_instance.sh
+++ scripts/scripts.d/50_eoxs_instance.sh
@@ -696,6 +696,7 @@ END
 $ a
 # ALLAUTH URLS - BEGIN - Do not edit or remove this line!
 import eoxs_allauth.views
+import eoxs_allauth.urls
 from vires.client_state import parse_client_state
 from django.views.generic import TemplateView
 
@@ -703,21 +704,7 @@ urlpatterns += patterns('',
     url(r'^/?$', eoxs_allauth.views.workspace(parse_client_state)),
     url(r'^ows$', eoxs_allauth.views.wrapped_ows),
     url(r'^accounts/', include('eoxs_allauth.urls')),
-    url(
-        r'^accounts/faq$',
-        TemplateView.as_view(template_name='account/faq.html'),
-        name='faq'
-    ),
-    url(
-        r'^accounts/datatc$',
-        TemplateView.as_view(template_name='account/datatc.html'),
-        name='datatc'
-    ),
-    url(
-        r'^accounts/servicetc$',
-        TemplateView.as_view(template_name='account/servicetc.html'),
-        name='servicetc'
-    ),
+    *eoxs_allauth.urls.document_urlpatterns
 )
 # ALLAUTH URLS - END - Do not edit or remove this line!
 .
