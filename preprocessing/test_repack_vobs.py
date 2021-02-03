deada0859907dc2c3ba65ec3537feb18104e41bc
Martin Paces <martin.paces@eox.at>
2021-02-03 11:25:22 +0100

    Adding support for the secular variation.

--- preprocessing/test_repack_vobs.py
+++ preprocessing/test_repack_vobs.py
@@ -33,12 +33,6 @@ from numpy.testing import assert_equal #, assert_allclose
 from spacepy import pycdf
 
 
-#DEF_PRECISION = {'atol': 0, 'rtol': 0}
-#PRECISION = {
-#    'Timestamp': {'atol': 1e-3, 'rtol': 0},
-#}
-
-
 class CommandError(Exception):
     """ Command error exception. """
 
@@ -92,8 +86,45 @@ def test_converted_vobs(filename_source, filename_tested):
             error_count += compare_global_attributes(
                 cdf_src.attrs, cdf_tested.attrs
             )
-            error_count += compare_variables(cdf_src, cdf_tested)
-            error_count += verify_site_codes(cdf_tested)
+            error_count += compare_variables(
+                cdf_src, cdf_tested,
+                'Timestamp', ['Latitude', 'Longitude', 'Radius'],
+                {
+                    'Timestamp': 'Timestamp',
+                    'Latitude': 'Latitude',
+                    'Longitude': 'Longitude',
+                    'Radius': 'Radius',
+                    'B_CF': 'B_CF',
+                    'B_OB': 'B_OB',
+                    'sigma_CF': 'sigma_CF',
+                    'sigma_OB': 'sigma_OB',
+                },
+            )
+            error_count += compare_variables(
+                cdf_src, cdf_tested,
+                'Timestamp_SV', ['Latitude_SV', 'Longitude_SV', 'Radius_SV'],
+                {
+                    'Timestamp_SV': 'Timestamp_SV',
+                    'Latitude_SV': 'Latitude',
+                    'Longitude_SV': 'Longitude',
+                    'Radius_SV': 'Radius',
+                    'B_SV': 'B_SV',
+                    'sigma_SV': 'sigma_SV',
+                },
+                dst_is_subset_of_src=True,
+            )
+            error_count += verify_site_codes(
+                cdf_tested, 'SiteCode', 'Latitude', 'Longitude'
+            )
+            error_count += verify_site_codes(
+                cdf_tested, 'SiteCode_SV', 'Latitude_SV', 'Longitude_SV'
+            )
+            error_count += verify_index_ranges(
+                cdf_tested, 'SiteCode', 'SITE_CODES', 'INDEX_RANGES'
+            )
+            error_count += verify_index_ranges(
+                cdf_tested, 'SiteCode_SV', 'SITE_CODES', 'INDEX_RANGES_SV'
+            )
     if error_count:
         error(f"{filename_tested} has issues!")
     else:
@@ -101,7 +132,28 @@ def test_converted_vobs(filename_source, filename_tested):
     return error_count > 0
 
 
-def verify_site_codes(cdf):
+def verify_index_ranges(cdf, site_code_variable, site_code_attribute, index_range_attribute):
+    """ Verify site index ranges. """
+    sites = list(cdf.attrs[site_code_attribute])
+    ranges = [tuple(item) for item in list(cdf.attrs[index_range_attribute])]
+
+    ref_ranges = {}
+    for idx, code in enumerate(cdf[site_code_variable][...]):
+        start, _ = ref_ranges.get(code) or (idx, None)
+        ref_ranges[code] = (start, idx + 1)
+
+    if sites != list(ref_ranges):
+        error(f"Invalid {site_code_attribute} codes!")
+        return  1
+
+    if ranges != list(ref_ranges.values()):
+        error(f"Incorrect {index_range_attribute} ranges!")
+        return  1
+
+    return 0
+
+
+def verify_site_codes(cdf, site_code_variable, latitude_variable, longitude_variable):
     """ Verify site labels. """
 
     def _get_site_code(latitude, longitude):
@@ -110,65 +162,68 @@ def verify_site_codes(cdf):
             "E" if longitude >= 0.0 else "W", abs(int(round(longitude))),
         ))
 
-    key = 'SiteCode'
-
-    if key not in cdf:
+    if site_code_variable not in cdf:
         error("Missing {key} variable!")
         return 1
 
     site_codes = asarray([
         _get_site_code(lat, lon) for lat, lon
-        in zip(cdf['Latitude'][...], cdf['Longitude'][...])
+        in zip(cdf[latitude_variable][...], cdf[longitude_variable][...])
     ])
 
     try:
-        assert_equal(site_codes, cdf['SiteCode'][...])
+        assert_equal(site_codes, cdf[site_code_variable][...])
     except AssertionError:
-        error(f"Invalid {key} values detected!")
+        error(f"Invalid {site_code_variable} values detected!")
         return 1
 
     return 0
 
 
-def compare_variables(cdf_src, cdf_dst):
+def compare_variables(cdf_src, cdf_dst, time_variable, location_variables,
+                      tested_variables, dst_is_subset_of_src=False):
     """ Compare variables. """
     error_count = 0
-    index_src2dst, index_dst2src = _find_mapping(cdf_src, cdf_dst)
+    _vars = [time_variable] + location_variables
+    index_src2dst, index_dst2src = _find_mapping(
+        cdf_src, cdf_dst, [tested_variables[v] for v in _vars], _vars,
+    )
 
-    if cdf_src.raw_var('Timestamp').shape != index_dst2src.shape:
+    mask_src = index_dst2src != -1
+    mask_dst = index_src2dst != -1
+
+    if not mask_dst.all():
         error_count += 1
-        error("Ambiguous tested to source element mapping!")
+        error("Impossible source to tested element mapping!")
 
-    if cdf_dst.raw_var('Timestamp').shape != index_dst2src.shape:
+    if not mask_src.all() and not dst_is_subset_of_src:
         error_count += 1
-        error("Ambiguous source to tested element mapping!")
+        error("Impossible tested to source element mapping!")
 
-    idx = arange(len(cdf_src.raw_var('Timestamp')))
+    idx_src = arange(mask_src.size)
+    idx_dst = arange(mask_dst.size)
     if (
-            (idx != idx[index_src2dst][index_dst2src]).any() or
-            (idx != idx[index_dst2src][index_src2dst]).any()
+            (idx_src[mask_src] != idx_src[index_src2dst][index_dst2src[mask_src]]).any() or
+            (idx_dst != idx_dst[index_dst2src][index_src2dst]).any()
         ):
         error_count += 1
         error("Ambiguous element mapping!")
 
-    excluded = ['SiteCode']
-    for key in cdf_dst:
-        if key in excluded:
-            continue
-        data_ref = cdf_src.raw_var(key)[...]
-        data_tested = cdf_dst.raw_var(key)[...][index_src2dst]
-        #data_tested = cdf_dst.raw_var(key)[...]#[index_src2dst]
-        if key.startswith('B_') or key.startswith('sigma_'):
+    # compare mapping
+    for var_dst, var_src in tested_variables.items():
+        data_ref = cdf_src.raw_var(var_src)[...]
+        data_tested = cdf_dst.raw_var(var_dst)[...]
+        if var_dst.startswith('B_') or var_dst.startswith('sigma_'):
             data_tested = _convert_nec_to_rtp(data_tested)
         try:
-            assert_equal(data_ref, data_tested)
-            #assert_allclose(
-            #    data_ref, data_tested, equal_nan=True,
-            #    **PRECISION.get(key, DEF_PRECISION)
-            #)
+            assert_equal(data_ref[index_src2dst], data_tested)
+            assert_equal(
+                data_ref[:mask_src.size][mask_src],
+                data_tested[index_dst2src[mask_src]]
+            )
         except AssertionError:
             error_count += 1
-            error(f"{key} data are not equal!")
+            error(f"{var_dst} values differ!")
 
     return error_count
 
@@ -177,34 +232,30 @@ def _convert_nec_to_rtp(data):
     return stack((-data[:, 2], -data[:, 0], data[:, 1]), axis=1)
 
 
-def _find_mapping(cdf_src, cdf_dst):
+def _find_mapping(cdf_src, cdf_dst, vars_src, vars_dst):
 
     def _to_int(item):
         time, lat, lon, rad = item
         return int(round(time)), int(round(lat)), int(round(lon)), int(round(rad))
 
-    src_items = [_to_int(item) for item in zip(
-        cdf_src.raw_var('Timestamp')[...],
-        cdf_src['Latitude'][...],
-        cdf_src['Longitude'][...],
-        cdf_src['Radius'][...],
-    )]
+    def _read_items(cdf, variables):
+        time, lat, lon, rad = variables
+        return [_to_int(item) for item in zip(
+            cdf.raw_var(time)[...],
+            cdf[lat][...],
+            cdf[lon][...],
+            cdf[rad][...],
+        )]
+
+    src_items = _read_items(cdf_src, vars_src)
     src_mapping = {item: idx for idx, item in enumerate(src_items)}
 
-    dst_items = [_to_int(item) for item in zip(
-        cdf_dst.raw_var('Timestamp')[...],
-        cdf_dst['Latitude'][...],
-        cdf_dst['Longitude'][...],
-        cdf_dst['Radius'][...],
-    )]
+    dst_items = _read_items(cdf_dst, vars_dst)
     dst_mapping = {item: idx for idx, item in enumerate(dst_items)}
 
-    index_src2dst = asarray([
-        dst_mapping[item] for item in src_items if item in dst_mapping
-    ])
-    index_dst2src = asarray([
-        src_mapping[item] for item in dst_items if item in src_mapping
-    ])
+    index_dst2src = asarray([dst_mapping.get(item, -1) for item in src_items])
+    index_src2dst = asarray([src_mapping.get(item, -1) for item in dst_items])
+
     return index_src2dst, index_dst2src
 
 
@@ -262,6 +313,7 @@ def cdf_open(filename, mode="r"):
     else:
         raise ValueError("Invalid mode value %r!" % mode)
     return cdf
+
 
 if __name__ == "__main__":
     try:
