13d26134b7393e31b43bbf89a60709432e0a4f66
pacesm <martin.paces@eox.at>
2021-02-04 11:10:37 +0100

    Support scripts for the Virtual Observatories products. (#78)
    * VOBS products re-packing script
    * VOBS products re-packing test script
    * VOBS Vagrant registration script

---
+++ preprocessing/repack_vobs.py
@@ -0,0 +1,379 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+#
+# Re-pack virtual observatory data to a new CDF file.
+#
+# Author: Martin Paces <martin.paces@eox.at>
+#-------------------------------------------------------------------------------
+# Copyright (C) 2021 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+import sys
+import ctypes
+from datetime import datetime #, timedelta
+from os import remove, rename
+from os.path import basename, exists, splitext
+from numpy import unique, concatenate, asarray, argsort, stack
+import spacepy
+from spacepy import pycdf
+
+
+GZIP_COMPRESSION = pycdf.const.GZIP_COMPRESSION
+GZIP_COMPRESSION_LEVEL4 = ctypes.c_long(4)
+GZIP_COMPRESSION_LEVEL = GZIP_COMPRESSION_LEVEL4
+
+CDF_CREATOR = "EOX:repack_vobs.py [%s-%s, libcdf-%s]" % (
+    spacepy.__name__, spacepy.__version__,
+    "%s.%s.%s-%s" % tuple(
+        v if isinstance(v, int) else v.decode('ascii')
+        for v in pycdf.lib.version
+    )
+)
+
+OBS_CODE_ATTRIBUTES = {
+    "UNITS": "-",
+    "DESCRIPTION": "Seven letter virtual observatory identification code associated with datum",
+    "FORMAT": "A7"
+}
+
+OBS_CODE_SV_ATTRIBUTES = {
+    "UNITS": "-",
+    "DESCRIPTION": "Secular variation seven letter virtual observatory identification code associated with datum",
+    "FORMAT": "A7"
+}
+
+OBS_CODES_ATTRIBUTE = "SITE_CODES"
+OBS_RANGES_ATTRIBUTE = "INDEX_RANGES"
+OBS_CODE_VARIABLE = "SiteCode"
+TIMESTAMP_VARIABLE = "Timestamp"
+LATITUDE_VARIABLE = "Latitude"
+LONGITUDE_VARIABLE = "Longitude"
+RADIUS_VARIABLE = "Radius"
+
+OBS_RANGES_SV_ATTRIBUTE = "INDEX_RANGES_SV"
+OBS_CODE_SV_VARIABLE = "SiteCode_SV"
+TIMESTAMP_SV_VARIABLE = "Timestamp_SV"
+LATITUDE_SV_VARIABLE = "Latitude_SV"
+LONGITUDE_SV_VARIABLE = "Longitude_SV"
+RADIUS_SV_VARIABLE = "Radius_SV"
+
+VARIABLES = [
+    TIMESTAMP_VARIABLE,
+    LATITUDE_VARIABLE,
+    LONGITUDE_VARIABLE,
+    RADIUS_VARIABLE,
+    "B_CF",
+    "B_OB",
+    "sigma_CF",
+    "sigma_OB",
+]
+
+VARIABLES_SV = [
+    TIMESTAMP_SV_VARIABLE,
+    LATITUDE_VARIABLE,
+    LONGITUDE_VARIABLE,
+    RADIUS_VARIABLE,
+    "B_SV",
+    "sigma_SV",
+]
+
+VARIABLES_SV_MAP = {
+    LATITUDE_VARIABLE: LATITUDE_SV_VARIABLE,
+    LONGITUDE_VARIABLE: LONGITUDE_SV_VARIABLE,
+    RADIUS_VARIABLE: RADIUS_SV_VARIABLE,
+}
+
+NEC_VARIABLES = [
+    "B_CF",
+    "B_OB",
+    "B_SV",
+    "sigma_CF",
+    "sigma_OB",
+    "sigma_SV",
+]
+
+CDF_CHAR_TYPE = pycdf.const.CDF_CHAR.value
+CDF_FLOAT_TYPE = pycdf.const.CDF_FLOAT.value
+CDF_DOUBLE_TYPE = pycdf.const.CDF_DOUBLE.value
+CDF_REAL8_TYPE = pycdf.const.CDF_REAL8.value # CDF_DOUBLE != CDF_REAL8
+CDF_REAL4_TYPE = pycdf.const.CDF_REAL4.value # CDF_FLOAT != CDF_REAL4
+
+TYPE_MAP = {
+    CDF_REAL8_TYPE: CDF_DOUBLE_TYPE,
+    CDF_REAL4_TYPE: CDF_FLOAT_TYPE,
+}
+
+# NOTE: There seems to be no way how to get the pad value via the pycdf API.
+TIMESTAMP_PAD_VALUE = 59958230400000.0 # 1900-01-01T00:00:00Z
+
+
+class CommandError(Exception):
+    """ Command error exception. """
+
+
+def usage(exename, file=sys.stderr):
+    """ Print usage. """
+    print("USAGE: %s <input> [<output>]" % basename(exename), file=file)
+    print("\n".join([
+        "DESCRIPTION:",
+        "  Re-pack virtual observatory product and save them into a new CDF file.",
+    ]), file=file)
+
+
+def parse_inputs(argv):
+    """ Parse input arguments. """
+    argv = argv + [None]
+    try:
+        input_ = argv[1]
+        output = argv[2]
+    except IndexError:
+        raise CommandError("Not enough input arguments!")
+    return input_, output or input_
+
+
+def main(filename_input, filename_output):
+    """ main subroutine """
+    filename_tmp = filename_output + ".tmp.cdf"
+
+    if exists(filename_tmp):
+        remove(filename_tmp)
+
+    try:
+        repack_vobs(filename_input, filename_tmp)
+        if exists(filename_tmp):
+            print("%s -> %s" % (filename_input, filename_output))
+            rename(filename_tmp, filename_output)
+        else:
+            print("%s skipped" % filename_input)
+    finally:
+        if exists(filename_tmp):
+            remove(filename_tmp)
+
+
+def is_repacked(cdf):
+    """ True if the CDF has been already repacked. """
+    return (
+        'CREATOR' in cdf.attrs
+        and str(cdf.attrs['CREATOR'][0]).startswith('EOX:repack_vobs.py')
+    )
+
+
+def repack_vobs(filename_input, filename_output):
+    """ Repack VOBS product file. """
+    extra_attributes = {
+        "ORIGINAL_PRODUCT_NAME": splitext(basename(filename_input))[0],
+    }
+    with cdf_open(filename_input) as cdf_src:
+
+        # skip already re-packed data
+        if is_repacked(cdf_src):
+            return
+
+        _, sites = extract_sites_labels(
+            cdf_src[LATITUDE_VARIABLE][...],
+            cdf_src[LONGITUDE_VARIABLE][...],
+            cdf_src[RADIUS_VARIABLE][...],
+        )
+        index, ranges = get_obs_index_and_ranges(
+            cdf_src.raw_var(TIMESTAMP_VARIABLE)[...], sites
+        )
+
+        times_sv = cdf_src.raw_var(TIMESTAMP_SV_VARIABLE)[...]
+        offset_sv = get_time_offset(times_sv, TIMESTAMP_PAD_VALUE)
+        index_sv, ranges_sv = get_obs_index_and_ranges(
+            times_sv[offset_sv:], sites[offset_sv:times_sv.size]
+        )
+        index_sv += offset_sv
+        del times_sv
+
+        assert list(ranges) == list(ranges_sv)
+
+        extra_attributes[OBS_CODES_ATTRIBUTE] = list(ranges)
+        extra_attributes[OBS_RANGES_ATTRIBUTE] = list(ranges.values())
+        extra_attributes[OBS_RANGES_SV_ATTRIBUTE] = list(ranges_sv.values())
+
+        with cdf_open(filename_output, "w") as cdf_dst:
+            _copy_attributes(cdf_dst, cdf_src)
+            _update_attributes(cdf_dst, extra_attributes)
+            _update_creator(cdf_dst)
+            _set_variable(
+                cdf_dst, OBS_CODE_VARIABLE, sites[index], CDF_CHAR_TYPE,
+                OBS_CODE_ATTRIBUTES
+            )
+            for variable in VARIABLES:
+                _copy_variable(cdf_dst, cdf_src, variable, variable, index)
+            _set_variable(
+                cdf_dst, OBS_CODE_SV_VARIABLE, sites[index_sv], CDF_CHAR_TYPE,
+                OBS_CODE_SV_ATTRIBUTES
+            )
+            for variable in VARIABLES_SV:
+                _copy_variable(
+                    cdf_dst, cdf_src, variable,
+                    VARIABLES_SV_MAP.get(variable, variable), index_sv
+                )
+
+
+def get_time_offset(times, nodata_value):
+    """ Get offset to the first valid time value. """
+    idx = (times != nodata_value).nonzero()[0]
+    return idx[0] if idx.size > 0 else times.size
+
+
+def get_obs_index_and_ranges_with_offset(times, codes, offset):
+    """ Get index sorting the arrays by observatory and time. """
+    index, ranges = get_obs_index_and_ranges(times[offset:], codes[offset:])
+    return index + offset, ranges
+
+
+def get_obs_index_and_ranges(times, codes):
+    """ Get index sorting the arrays by observatory and time. """
+    ranges = {}
+    indices = []
+    offset = 0
+    for code in get_obs_codes(codes):
+        index = get_obs_index(codes, code)
+        index = sort_by(times, index)
+        indices.append(index)
+        ranges[code] = (offset, offset + len(index))
+        offset += len(index)
+    return concatenate(indices), ranges
+
+
+def get_obs_index(codes, code):
+    """ Get index extracting records for the given observatory. """
+    return (codes == code).nonzero()[0]
+
+
+def sort_by(values, index):
+    """ Sort index by the given values. """
+    return index[values[index].argsort()]
+
+
+def get_obs_codes(codes):
+    """ Read available observatory codes from the source file. """
+    unique_codes, index = unique(codes, return_index=True)
+    return list(unique_codes[argsort(index)])
+
+
+def extract_sites_labels(latitudes, longitudes, radii):
+    """ Extract sites labels. """
+
+    def _get_site_code(latitude, longitude):
+        return ("%s%2.2d%s%3.3d" % (
+            "N" if latitude >= 0.0 else "S", abs(int(round(latitude))),
+            "E" if longitude >= 0.0 else "W", abs(int(round(longitude))),
+        )).encode('ascii')
+
+    site_map, sites = {}, []
+    for location in zip(latitudes, longitudes, radii):
+        site_code = site_map.get(location)
+        if not site_code:
+            latitude, longitude, _ = location
+            site_map[location] = site_code = _get_site_code(latitude, longitude)
+        sites.append(site_code)
+
+    return site_map, asarray(sites, 'S')
+
+
+def _update_attributes(cdf, attrs):
+    cdf.attrs.update(attrs or {})
+
+
+def _update_creator(cdf):
+    _update_attributes(cdf, {
+        "CREATOR": CDF_CREATOR,
+        "CREATED": (
+            datetime.utcnow().replace(microsecond=0)
+        ).isoformat() + "Z",
+    })
+
+
+def _copy_variable(cdf_dst, cdf_src, variable_src, variable_dst, index):
+    raw_var = cdf_src.raw_var(variable_src)
+    data = cdf_src.raw_var(variable_src)[...][index]
+    if variable_src in NEC_VARIABLES:
+        data = _covert_rtp_to_nec(data)
+    cdf_dst.new(
+        variable_dst,
+        data,
+        TYPE_MAP.get(raw_var.type(), raw_var.type()),
+        dims=data.shape[1:],
+        compress=GZIP_COMPRESSION,
+        compress_param=GZIP_COMPRESSION_LEVEL,
+    )
+    cdf_dst[variable_dst].attrs.update(raw_var.attrs)
+
+
+def _covert_rtp_to_nec(data):
+    return stack((-data[:, 1], +data[:, 2], -data[:, 0]), axis=1)
+
+
+def _set_variable(cdf_dst, variable, data, cdf_type, attrs=None):
+    cdf_dst.new(
+        variable,
+        data,
+        cdf_type,
+        dims=data.shape[1:],
+        compress=GZIP_COMPRESSION,
+        compress_param=GZIP_COMPRESSION_LEVEL,
+    )
+    if attrs:
+        cdf_dst[variable].attrs.update(attrs)
+
+
+def _copy_attributes(cdf_dst, cdf_src):
+    _update_attributes(cdf_dst, {
+        key: list(value)
+        for key, value in cdf_src.attrs.items()
+    })
+
+
+def cdf_open(filename, mode="r"):
+    """ Open a new or existing  CDF file.
+    Allowed modes are 'r' (read-only) and 'w' (read-write).
+    A new CDF file is created if the 'w' mode is chosen and the file does not
+    exist.
+    The returned object is a context manager which can be used with the `with`
+    command.
+
+    NOTE: for the newly created CDF files the pycdf.CDF adds the '.cdf'
+    extension to the filename if it does not end by this extension already.
+    """
+    if mode == "r":
+        cdf = pycdf.CDF(filename)
+    elif mode == "w":
+        if exists(filename):
+            cdf = pycdf.CDF(filename)
+            cdf.readonly(False)
+        else:
+            pycdf.lib.set_backward(False) # produce CDF version 3
+            cdf = pycdf.CDF(filename, "")
+    else:
+        raise ValueError("Invalid mode value %r!" % mode)
+    return cdf
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main(*parse_inputs(sys.argv)))
+    except CommandError as error:
+        print("ERROR: %s" % error, file=sys.stderr)
+        usage(sys.argv[0])
---
+++ preprocessing/test_repack_vobs.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+#
+# Test that if the re-packed VOBS file equals to the original file.
+#
+# Author: Martin Paces <martin.paces@eox.at>
+#-------------------------------------------------------------------------------
+# Copyright (C) 2021 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+import sys
+from os.path import basename, exists
+from numpy import asarray, stack, arange
+from numpy.testing import assert_equal #, assert_allclose
+from spacepy import pycdf
+
+
+class CommandError(Exception):
+    """ Command error exception. """
+
+
+def info(message):
+    """ Print info message. """
+    print(f"INFO: {message}")
+
+
+def warning(message):
+    """ Print warning message. """
+    print(f"WARNING: {message}")
+
+
+def error(message):
+    """ Print error message. """
+    print(f"ERROR: {message}")
+
+
+def usage(exename, file=sys.stderr):
+    """ Print usage. """
+    print("USAGE: %s <source-CDF> <re-packed-CDF>" % basename(exename), file=file)
+    print("\n".join([
+        "DESCRIPTION:",
+        "  Test the re-pack virtual observatory product against its source.",
+    ]), file=file)
+
+
+def parse_inputs(argv):
+    """ Parse input arguments. """
+    argv = argv + [None]
+    try:
+        source = argv[1]
+        tested = argv[2]
+    except IndexError:
+        raise CommandError("Not enough input arguments!")
+    return source, tested
+
+
+def main(filename_source, filename_tested):
+    """ main subroutine """
+    return test_converted_vobs(filename_source, filename_tested)
+
+
+def test_converted_vobs(filename_source, filename_tested):
+    """ Compare converted VOBS file to its source and report differences. """
+    info(f"Comparing {filename_tested} to {filename_source} ...")
+    error_count = 0
+    with cdf_open(filename_source) as cdf_src:
+        with cdf_open(filename_tested) as cdf_tested:
+            error_count += compare_global_attributes(
+                cdf_src.attrs, cdf_tested.attrs
+            )
+            error_count += compare_variables(
+                cdf_src, cdf_tested,
+                'Timestamp', ['Latitude', 'Longitude', 'Radius'],
+                {
+                    'Timestamp': 'Timestamp',
+                    'Latitude': 'Latitude',
+                    'Longitude': 'Longitude',
+                    'Radius': 'Radius',
+                    'B_CF': 'B_CF',
+                    'B_OB': 'B_OB',
+                    'sigma_CF': 'sigma_CF',
+                    'sigma_OB': 'sigma_OB',
+                },
+            )
+            error_count += compare_variables(
+                cdf_src, cdf_tested,
+                'Timestamp_SV', ['Latitude_SV', 'Longitude_SV', 'Radius_SV'],
+                {
+                    'Timestamp_SV': 'Timestamp_SV',
+                    'Latitude_SV': 'Latitude',
+                    'Longitude_SV': 'Longitude',
+                    'Radius_SV': 'Radius',
+                    'B_SV': 'B_SV',
+                    'sigma_SV': 'sigma_SV',
+                },
+                dst_is_subset_of_src=True,
+            )
+            error_count += verify_site_codes(
+                cdf_tested, 'SiteCode', 'Latitude', 'Longitude'
+            )
+            error_count += verify_site_codes(
+                cdf_tested, 'SiteCode_SV', 'Latitude_SV', 'Longitude_SV'
+            )
+            error_count += verify_index_ranges(
+                cdf_tested, 'SiteCode', 'SITE_CODES', 'INDEX_RANGES'
+            )
+            error_count += verify_index_ranges(
+                cdf_tested, 'SiteCode_SV', 'SITE_CODES', 'INDEX_RANGES_SV'
+            )
+    if error_count:
+        error(f"{filename_tested} has issues!")
+    else:
+        info(f"{filename_tested} is correct.")
+    return error_count > 0
+
+
+def verify_index_ranges(cdf, site_code_variable, site_code_attribute, index_range_attribute):
+    """ Verify site index ranges. """
+    sites = list(cdf.attrs[site_code_attribute])
+    ranges = [tuple(item) for item in list(cdf.attrs[index_range_attribute])]
+
+    ref_ranges = {}
+    for idx, code in enumerate(cdf[site_code_variable][...]):
+        start, _ = ref_ranges.get(code) or (idx, None)
+        ref_ranges[code] = (start, idx + 1)
+
+    if sites != list(ref_ranges):
+        error(f"Invalid {site_code_attribute} codes!")
+        return  1
+
+    if ranges != list(ref_ranges.values()):
+        error(f"Incorrect {index_range_attribute} ranges!")
+        return  1
+
+    return 0
+
+
+def verify_site_codes(cdf, site_code_variable, latitude_variable, longitude_variable):
+    """ Verify site labels. """
+
+    def _get_site_code(latitude, longitude):
+        return ("%s%2.2d%s%3.3d" % (
+            "N" if latitude >= 0.0 else "S", abs(int(round(latitude))),
+            "E" if longitude >= 0.0 else "W", abs(int(round(longitude))),
+        ))
+
+    if site_code_variable not in cdf:
+        error("Missing {key} variable!")
+        return 1
+
+    site_codes = asarray([
+        _get_site_code(lat, lon) for lat, lon
+        in zip(cdf[latitude_variable][...], cdf[longitude_variable][...])
+    ])
+
+    try:
+        assert_equal(site_codes, cdf[site_code_variable][...])
+    except AssertionError:
+        error(f"Invalid {site_code_variable} values detected!")
+        return 1
+
+    return 0
+
+
+def compare_variables(cdf_src, cdf_dst, time_variable, location_variables,
+                      tested_variables, dst_is_subset_of_src=False):
+    """ Compare variables. """
+    error_count = 0
+    _vars = [time_variable] + location_variables
+    index_src2dst, index_dst2src = _find_mapping(
+        cdf_src, cdf_dst, [tested_variables[v] for v in _vars], _vars,
+    )
+
+    mask_src = index_dst2src != -1
+    mask_dst = index_src2dst != -1
+
+    if not mask_dst.all():
+        error_count += 1
+        error("Impossible source to tested element mapping!")
+
+    if not mask_src.all() and not dst_is_subset_of_src:
+        error_count += 1
+        error("Impossible tested to source element mapping!")
+
+    idx_src = arange(mask_src.size)
+    idx_dst = arange(mask_dst.size)
+    if (
+            (idx_src[mask_src] != idx_src[index_src2dst][index_dst2src[mask_src]]).any() or
+            (idx_dst != idx_dst[index_dst2src][index_src2dst]).any()
+        ):
+        error_count += 1
+        error("Ambiguous element mapping!")
+
+    # compare mapping
+    for var_dst, var_src in tested_variables.items():
+        data_ref = cdf_src.raw_var(var_src)[...]
+        data_tested = cdf_dst.raw_var(var_dst)[...]
+        if var_dst.startswith('B_') or var_dst.startswith('sigma_'):
+            data_tested = _convert_nec_to_rtp(data_tested)
+        try:
+            assert_equal(data_ref[index_src2dst], data_tested)
+            assert_equal(
+                data_ref[:mask_src.size][mask_src],
+                data_tested[index_dst2src[mask_src]]
+            )
+        except AssertionError:
+            error_count += 1
+            error(f"{var_dst} values differ!")
+
+    return error_count
+
+
+def _convert_nec_to_rtp(data):
+    return stack((-data[:, 2], -data[:, 0], data[:, 1]), axis=1)
+
+
+def _find_mapping(cdf_src, cdf_dst, vars_src, vars_dst):
+
+    def _to_int(item):
+        time, lat, lon, rad = item
+        return int(round(time)), int(round(lat)), int(round(lon)), int(round(rad))
+
+    def _read_items(cdf, variables):
+        time, lat, lon, rad = variables
+        return [_to_int(item) for item in zip(
+            cdf.raw_var(time)[...],
+            cdf[lat][...],
+            cdf[lon][...],
+            cdf[rad][...],
+        )]
+
+    src_items = _read_items(cdf_src, vars_src)
+    src_mapping = {item: idx for idx, item in enumerate(src_items)}
+
+    dst_items = _read_items(cdf_dst, vars_dst)
+    dst_mapping = {item: idx for idx, item in enumerate(dst_items)}
+
+    index_dst2src = asarray([dst_mapping.get(item, -1) for item in src_items])
+    index_src2dst = asarray([src_mapping.get(item, -1) for item in dst_items])
+
+    return index_src2dst, index_dst2src
+
+
+def compare_global_attributes(src_attrs, dst_attrs):
+    """ Compare global attributes. """
+    error_count = 0
+    excluded = ['ORIGINAL_PRODUCT_NAME', 'CREATOR']
+    for key in src_attrs:
+        if key in excluded:
+            continue
+
+        if key not in dst_attrs:
+            error_count += 1
+            error(f"Missing {key} global attribute!")
+            continue
+        items0 = list(src_attrs[key])
+        items1 = list(dst_attrs[key])
+        if len(items0) != len(items1):
+            error_count += 1
+            error(
+                f"Mismatch of the count of the {key} global attribute"
+                f" elements {len(items0)} != {len(items1)}"
+            )
+
+        for idx, (item0, item1) in enumerate(zip(items0, items1)):
+            if item0 != item1:
+                error_count += 1
+                error(
+                    f"Mismatch of the global attribute {key}[{idx}]!"
+                    f" {item0!r} != {item1!r}"
+                )
+    return error_count
+
+
+def cdf_open(filename, mode="r"):
+    """ Open a new or existing  CDF file.
+    Allowed modes are 'r' (read-only) and 'w' (read-write).
+    A new CDF file is created if the 'w' mode is chosen and the file does not
+    exist.
+    The returned object is a context manager which can be used with the `with`
+    command.
+
+    NOTE: for the newly created CDF files the pycdf.CDF adds the '.cdf'
+    extension to the filename if it does not end by this extension already.
+    """
+    if mode == "r":
+        cdf = pycdf.CDF(filename)
+    elif mode == "w":
+        if exists(filename):
+            cdf = pycdf.CDF(filename)
+            cdf.readonly(False)
+        else:
+            pycdf.lib.set_backward(False) # produce CDF version 3
+            cdf = pycdf.CDF(filename, "")
+    else:
+        raise ValueError("Invalid mode value %r!" % mode)
+    return cdf
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main(*parse_inputs(sys.argv)))
+    except CommandError as error:
+        print("ERROR: %s" % error, file=sys.stderr)
+        usage(sys.argv[0])
