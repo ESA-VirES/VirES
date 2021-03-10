b351e49dc3ecece8b36ec50d8db81c00393abb10
pacesm <martin.paces@eox.at>
2021-03-10 17:30:13 +0100

    Fixing GDAL libary path on the DISC machine. (#80)
    * Setting GDAL_LIBRARY_PATH.

--- scripts/scripts.d/51_eoxs_instance_production.sh
+++ scripts/scripts.d/51_eoxs_instance_production.sh
@@ -96,6 +96,10 @@ cat > "$SETTINGS" <<END
 
 from os.path import join, abspath, dirname
 
+# temporary workaround resolving conflict with gdal32 from pgdg-common
+# remove once the DB instance is gone
+GDAL_LIBRARY_PATH = "/usr/lib64/libgdal.so.1"
+
 DEBUG = False
 
 PROJECT_DIR = dirname(abspath(__file__))
