04af25de1be8349fe457d0de23f2f5f8d245422e
Martin Paces <martin.paces@eox.at>
2021-07-11 16:37:45 +0200

    Changing to new style Django URL patterns.

--- scripts/scripts.d/51_eoxs_instance.sh
+++ scripts/scripts.d/51_eoxs_instance.sh
@@ -339,15 +339,15 @@ END
 # STEP 2-2: INSTANCE CONFIGURATION - optional - no authentication
 
 [ "$CONFIGURE_ALLAUTH" != "YES" ] && cat > "$URLS" <<END
-from django.conf.urls import url
+from django.urls import include, path, re_path
 from eoxserver.services.views import ows
 from vires.views import custom_data #, custom_model, client_state
 
 urlpatterns = [
-    url(r'^ows$', ows, name="ows"),
-    url(r'^custom_data/(?P<identifier>[0-9a-f-]{36,36})?$', custom_data),
-    #url(r'^custom_model/(?P<identifier>[0-9a-f-]{36,36})?$', custom_model),
-    #url(r'^client_state/(?P<identifier>[0-9a-f-]{36,36})?$', client_state),
+    path('ows', ows, name="ows"),
+    re_path(r'^custom_data/(?P<identifier>[0-9a-f-]{36,36})?$', custom_data),
+    #re_path(r'^custom_model/(?P<identifier>[0-9a-f-]{36,36})?$', custom_model),
+    #re_path(r'^client_state/(?P<identifier>[0-9a-f-]{36,36})?$', client_state),
 ]
 END
 
@@ -355,7 +355,7 @@ END
 # STEP 2-3: INSTANCE CONFIGURATION - optional - authentication enabled
 
 [ "$CONFIGURE_ALLAUTH" == "YES" ] && cat > "$URLS" <<END
-from django.conf.urls import include, url
+from django.urls import include, path, re_path
 from eoxserver.services.views import ows
 from eoxs_allauth.views import wrap_protected_api, wrap_open_api, workspace
 from eoxs_allauth.urls import document_urlpatterns
@@ -363,12 +363,12 @@ from vires.client_state import parse_client_state
 from vires.views import custom_data #, custom_model, client_state
 
 urlpatterns = [
-    url(r'^$', workspace(parse_client_state), name="workspace"),
-    url(r'^ows$', wrap_protected_api(ows), name="ows"),
-    url(r'^accounts/', include('eoxs_allauth.urls')),
-    url(r'^custom_data/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(custom_data)),
-    #url(r'^custom_model/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(custom_model)),
-    #url(r'^client_state/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(client_state)),
+    path('', workspace(parse_client_state), name="workspace"),
+    path('ows', wrap_protected_api(ows), name="ows"),
+    path('accounts/', include('eoxs_allauth.urls')),
+    re_path(r'^custom_data/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(custom_data)),
+    #re_path(r'^custom_model/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(custom_model)),
+    #re_path(r'^client_state/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(client_state)),
 ] + document_urlpatterns
 END
 
