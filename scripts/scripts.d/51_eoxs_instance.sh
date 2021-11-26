bfe435453b46c709051212e05868bab4a0e3c8a9
Martin Paces <martin.paces@eox.at>
2021-11-26 09:19:25 +0100

    HAPI facade configuration

--- scripts/scripts.d/51_eoxs_instance.sh
+++ scripts/scripts.d/51_eoxs_instance.sh
@@ -99,6 +99,12 @@ PROJECT_URL_PREFIX = ''
 MANAGERS = ADMINS = (
 )
 
+HAPI_ABOUT = {
+  'id': 'VirES-dev',
+  'contact': 'feedback@vires.services',
+  'description': 'Web API for the ESA Swarm mission products.',
+}
+
 DATABASES = {
     'default': {
         'ENGINE': '$DBENGINE',
@@ -343,9 +349,11 @@ END
 from django.urls import include, path, re_path
 from eoxserver.services.views import ows
 from vires.views import custom_data #, custom_model, client_state
+from vires.hapi.urls import urlpatterns as hapi_urlpatterns
 
 urlpatterns = [
     path('ows', ows, name="ows"),
+    path('hapi/', include(hapi_urlpatterns)),
     re_path(r'^custom_data/(?P<identifier>[0-9a-f-]{36,36})?$', custom_data),
     #re_path(r'^custom_model/(?P<identifier>[0-9a-f-]{36,36})?$', custom_model),
     #re_path(r'^client_state/(?P<identifier>[0-9a-f-]{36,36})?$', client_state),
@@ -362,10 +370,12 @@ from eoxs_allauth.views import wrap_protected_api, wrap_open_api, workspace
 from eoxs_allauth.urls import document_urlpatterns
 from vires.client_state import parse_client_state
 from vires.views import custom_data #, custom_model, client_state
+from vires.hapi.urls import urlpatterns as hapi_urlpatterns
 
 urlpatterns = [
     path('', workspace(parse_client_state), name="workspace"),
     path('ows', wrap_protected_api(ows), name="ows"),
+    path('hapi/', include(hapi_urlpatterns)),
     path('accounts/', include('eoxs_allauth.urls')),
     re_path(r'^custom_data/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(custom_data)),
     #re_path(r'^custom_model/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(custom_model)),
--- scripts/scripts.d/51_eoxs_instance_production.sh
+++ scripts/scripts.d/51_eoxs_instance_production.sh
@@ -105,6 +105,12 @@ MANAGERS = ADMINS = (
     $_ADMINS
 )
 
+HAPI_ABOUT = {
+  'id': 'VirES-dev',
+  'contact': 'feedback@vires.services',
+  'description': 'Web API for the ESA Swarm mission products.',
+}
+
 DATABASES = {
     'default': {
         'ENGINE': '$DBENGINE',
@@ -369,9 +375,11 @@ END
 from django.urls import include, path, re_path
 from eoxserver.services.views import ows
 from vires.views import custom_data #, custom_model, client_state
+from vires.hapi.urls import urlpatterns as hapi_urlpatterns
 
 urlpatterns = [
     path('ows', ows, name="ows"),
+    path('hapi/', include(hapi_urlpatterns)),
     re_path(r'^custom_data/(?P<identifier>[0-9a-f-]{36,36})?$', custom_data),
     #re_path(r'^custom_model/(?P<identifier>[0-9a-f-]{36,36})?$', custom_model),
     #re_path(r'^client_state/(?P<identifier>[0-9a-f-]{36,36})?$', client_state),
@@ -388,10 +396,12 @@ from eoxs_allauth.views import wrap_protected_api, wrap_open_api, workspace
 from eoxs_allauth.urls import document_urlpatterns
 from vires.client_state import parse_client_state
 from vires.views import custom_data #, custom_model, client_state
+from vires.hapi.urls import urlpatterns as hapi_urlpatterns
 
 urlpatterns = [
     path('', workspace(parse_client_state), name="workspace"),
     path('ows', wrap_protected_api(ows), name="ows"),
+    path('hapi/', include(hapi_urlpatterns)),
     path('accounts/', include('eoxs_allauth.urls')),
     re_path(r'^custom_data/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(custom_data)),
     #re_path(r'^custom_model/(?P<identifier>[0-9a-f-]{36,36})?$', wrap_protected_api(custom_model)),
