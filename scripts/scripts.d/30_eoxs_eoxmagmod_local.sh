84ee8a9049d463541fd1fc9658668323abfb5b9d
Martin Paces <martin.paces@eox.at>
2022-12-08 16:04:06 +0100

    Using newer compiler for eoxmagmod compilation.

--- scripts/scripts.d/30_eoxs_eoxmagmod_local.sh
+++ scripts/scripts.d/30_eoxs_eoxmagmod_local.sh
@@ -6,8 +6,11 @@
 #-------------------------------------------------------------------------------
 # Copyright (C) 2015 EOX IT Services GmbH
 
+. `dirname $0`/../lib_util.sh
 . `dirname $0`/../lib_logging.sh
 . `dirname $0`/../lib_python_venv.sh
+
+required_variables DEVTOOLSET_VERSION GCC_MARCH GCC_MTUNE
 
 SOURCE="${EOXMAGMOD_SOURCE_PATH:-/usr/local/eoxmagmod}"
 
@@ -21,4 +24,11 @@ yum --assumeyes install qdipole qdipole-devel
 # get rid of the previous build
 [ -d "$SOURCE/build" ] && rm -fvR "$SOURCE/build"
 [ -d "$SOURCE/dist" ] && rm -fvR "$SOURCE/dist"
+
+# enable newer development tools
+. /opt/rh/devtoolset-${DEVTOOLSET_VERSION}/enable
+
+# custom compiler options
+export CFLAGS="-march=${GCC_MARCH} -mtune=${GCC_MTUNE} -ftree-vectorize"
+
 pip install $PIP_OPTIONS "$SOURCE"
--- scripts/scripts.d/30_eoxs_eoxmagmod_sdist.sh
+++ scripts/scripts.d/30_eoxs_eoxmagmod_sdist.sh
@@ -6,8 +6,11 @@
 #-------------------------------------------------------------------------------
 # Copyright (C) 2015 EOX IT Services GmbH
 
+. `dirname $0`/../lib_util.sh
 . `dirname $0`/../lib_logging.sh
 . `dirname $0`/../lib_python_venv.sh
+
+required_variables DEVTOOLSET_VERSION GCC_MARCH GCC_MTUNE
 
 SOURCE="https://github.com/ESA-VirES/MagneticModel/releases/download/eoxmagmod-0.11.0/eoxmagmod-0.11.0.tar.gz"
 
@@ -17,5 +20,11 @@ activate_venv "$VIRES_VENV_ROOT"
 
 # requires spacepy + cdf to be installed
 yum --assumeyes install qdipole qdipole-devel
+
+# enable newer development tools
+. /opt/rh/devtoolset-${DEVTOOLSET_VERSION}/enable
+
+# custom compiler options
+export CFLAGS="-march=${GCC_MARCH} -mtune=${GCC_MTUNE} -ftree-vectorize"
 
 pip install $PIP_OPTIONS "$SOURCE"
