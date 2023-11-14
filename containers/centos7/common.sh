cdd23dec14b190386d9b13112c735b49df73b05a
Martin Paces <martin.paces@eox.at>
2023-11-14 20:37:55 +0100

    Rearranging pod start up.

--- containers/centos7/common.sh
+++ containers/centos7/common.sh
@@ -18,5 +18,16 @@ jhub
 END
 }
 
+start_containers() {
+    $BIN_DIR/container database start \
+      && $BIN_DIR/container database exec /bin/sh -c 'while ! pg_isready ; do sleep 1 ; done' \
+      && $BIN_DIR/container database exec -i create_db < $VIRES_CONTAINER_ROOT/volumes/secrets/oauth.conf \
+      && $BIN_DIR/container database exec -i create_db < $VIRES_CONTAINER_ROOT/volumes/secrets/swarm.conf \
+      && $BIN_DIR/container oauth start \
+      && $BIN_DIR/container swarm start \
+      && $BIN_DIR/container jhub start \
+      && $BIN_DIR/container ingress start
+}
+
 VIRES_CONTAINER_ROOT="${VIRES_CONTAINER_ROOT:-.}"
 [ -f "$VIRES_CONTAINER_ROOT/tag.conf" ] && . "$VIRES_CONTAINER_ROOT/tag.conf"
