18178873964778cf62d64453fc26bcaadbd8d3ee
Martin Paces <martin.paces@eox.at>
2024-06-11 17:53:55 +0200

    AMPS - changing git reference

--- scripts/scripts.d/30_eoxs_pyamps.sh
+++ scripts/scripts.d/30_eoxs_pyamps.sh
@@ -30,5 +30,5 @@ END
 # installing pyAMPS directly from the Git repo
 #pip install $PIP_OPTIONS git+https://github.com/klaundal/pyAMPS.git@1.6#pyamps
 
-# temporary fix of PyAMPS v1.6.0 - see https://github.com/klaundal/pyAMPS/pull/18
-pip install $PIP_OPTIONS git+https://github.com/pacesm/pyAMPS.git@fixing_filenames#pyamps
+# temporary fix of PyAMPS v1.6.0 - to be removed when PyAMPS >= v1.6.1 is released
+pip install $PIP_OPTIONS git+https://github.com/pacesm/pyAMPS.git@1.6_fixed#pyamps
