8cc04d162bcda82d7f83829fa0b05a8c92d5d766
Martin Paces <martin.paces@eox.at>
2024-10-06 10:18:50 +0200

    Fixing Redis container.

--- containers/debian12/redis/entrypoint.sh
+++ containers/debian12/redis/entrypoint.sh
@@ -4,8 +4,7 @@
 if [ -z "$*" ]
 then
     echo "Starting redis server ... "
-    echo "Logs are collected to $LOG_DIR/redis.log log file."
-    exec redis-server 2>&1 | tee "$LOG_DIR/redis.log"
+    exec redis-server /etc/redis/redis.conf --daemonize no
 else
     exec "$@"
 fi
